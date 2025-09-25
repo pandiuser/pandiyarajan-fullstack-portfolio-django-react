575720037f3e0a5b18a071225160d566a5ae2a65
pandiuser <pandiyarajans372@gmail.com>
2025-09-26 01:09:25 +0530

    CORS_ALLOW_ALL_ORIGINS

--- api/api/settings.py
+++ api/api/settings.py
@@ -42,7 +42,7 @@ DEBUG = True
 # ALLOWED_HOSTS = os.getenv(
 #     'ALLOWED_HOSTS').split(',')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","0.0.0.0","https://pandiyarajan-fullstack-portfolio-django-igcq.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","0.0.0.0","pandiyarajan-fullstack-portfolio-django-igcq.onrender.com"]
 
 
 # Application definition
